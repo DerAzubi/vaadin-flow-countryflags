5366171627884b50fac524f3dc823acc4453e196
DerAzubi <DerAzubi@users.noreply.github.com>
2019-01-27 20:36:12 +0100

    init commit

---
+++ src/main/java/de/jfancy/countryflags/ui/component/CountryFlags.java
@@ -0,0 +1,243 @@
+package de.jfancy.countryflags.ui.component;
+
+import com.vaadin.flow.component.html.Image;
+
+/**
+ * 195 Country Flags
+ * 
+ * @author Marcus
+ *
+ */
+public enum CountryFlags {
+
+    AFGHANISTAN("icons/flags/Afghanistan.png",""),
+    ALBANIA("icons/flags/Albania.png",""),
+    ALGERIA("icons/flags/Algeria.png",""),
+    ANDORRA("icons/flags/Andorra.png",""),
+    ANTIGUA_AND_BARBUDA("icons/flags/Antigua-and-Barbuda.png",""),
+    ARGENTINA("icons/flags/Argentina.png",""),
+    ARMENIA("icons/flags/Armenia.png",""),
+    AUSTRALIA("icons/flags/Australia.png",""),
+    AUSTRIA("icons/flags/Austria.png",""),
+    AZERBAIJAN("icons/flags/Azerbaijan.png",""),
+    BAHAMAS("icons/flags/Bahamas.png",""),
+    BAHRAIN("icons/flags/Bahrain.png",""),
+    BANGLADESH("icons/flags/Bangladesh.png",""),
+    BARBADOS("icons/flags/Barbados.png",""),
+    BELARUS("icons/flags/Belarus.png",""),
+    BELGIUM("icons/flags/Belgium.png",""),
+    BELIZE("icons/flags/Belize.png",""),
+    BENIN("icons/flags/Benin.png",""),
+    BHUTAN("icons/flags/Bhutan.png",""),
+    BOLIVIA("icons/flags/Bolivia.png",""),
+    BOSNIA_AND_HERZEGOVINA("icons/flags/Bosnia-and-Herzegovina.png",""),
+    BOTSWANA("icons/flags/Botswana.png",""),
+    BRAZIL("icons/flags/Brazil.png",""),
+    BRUNEI("icons/flags/Brunei.png",""),
+    BULGARIA("icons/flags/Bulgaria.png",""),
+    BURKINA_FASO("icons/flags/Burkina-Faso.png",""),
+    BURUNDI("icons/flags/Burundi.png",""),
+    CAMBODIA("icons/flags/Cambodia.png",""),
+    CAMEROON("icons/flags/Cameroon.png",""),
+    CANADA("icons/flags/Canada.png",""),
+    CAPE_VERDE("icons/flags/Cape-Verde.png",""),
+    CENTRAL_AFRICAN_REPUBLIC("icons/flags/Central-African-Republic.png",""),
+    CHAD("icons/flags/Chad.png",""),
+    CHILE("icons/flags/Chile.png",""),
+    CHINA("icons/flags/China.png",""),
+    COLOMBIA("icons/flags/Colombia.png",""),
+    COMOROS("icons/flags/Comoros.png",""),
+    CONGO_DEMOCRATIC("icons/flags/Congo-(Democratic).png",""),
+    CONGO_REPUBLIC("icons/flags/Congo-(Republic).png",""),
+    COSTA_RICA("icons/flags/Costa-Rica.png",""),
+    COTE_DIVOIRE("icons/flags/Cote-d'Ivoire.png",""),
+    CROATIA("icons/flags/Croatia.png",""),
+    CUBA("icons/flags/Cuba.png",""),
+    CYPRUS("icons/flags/Cyprus.png",""),
+    CZECH_REPUBLIC("icons/flags/Czech-Republic.png",""),
+    DENMARK("icons/flags/Denmark.png",""),
+    DJIBOUTI("icons/flags/Djibouti.png",""),
+    DOMINICA("icons/flags/Dominica.png",""),
+    DOMINICAN_REPUBLIC("icons/flags/Dominican-Republic.png",""),
+    EAST_TIMOR("icons/flags/East-Timor.png",""),
+    ECUADOR("icons/flags/Ecuador.png",""),
+    EGYPT("icons/flags/Egypt.png",""),
+    EL_SALVADOR("icons/flags/El-Salvador.png",""),
+    EQUATORIAL_GUINEA("icons/flags/Equatorial-Guinea.png",""),
+    ERITREA("icons/flags/Eritrea.png",""),
+    ESTONIA("icons/flags/Estonia.png",""),
+    ETHIOPIA("icons/flags/Ethiopia.png",""),
+    FIJI("icons/flags/Fiji.png",""),
+    FINLAND("icons/flags/Finland.png",""),
+    FRANCE("icons/flags/France.png",""),
+    GABON("icons/flags/Gabon.png",""),
+    GAMBIA("icons/flags/Gambia.png",""),
+    GEORGIA("icons/flags/Georgia.png",""),
+    GERMANY("icons/flags/Germany.png",""),
+    GHANA("icons/flags/Ghana.png",""),
+    GRECEE("icons/flags/Grecee.png",""),
+    GRENADA("icons/flags/Grenada.png",""),
+    GUATEMALA("icons/flags/Guatemala.png",""),
+    GUINEA_BISSAU("icons/flags/Guinea-Bissau.png",""),
+    GUINEA("icons/flags/Guinea.png",""),
+    GUYANA("icons/flags/Guyana.png",""),
+    HAITI("icons/flags/Haiti.png",""),
+    HONDURAS("icons/flags/Honduras.png",""),
+    HUNGARY("icons/flags/Hungary.png",""),
+    ICELAND("icons/flags/Iceland.png",""),
+    INDIA("icons/flags/India.png",""),
+    INDONESIA("icons/flags/Indonesia.png",""),
+    IRAN("icons/flags/Iran.png",""),
+    IRAQ("icons/flags/Iraq.png",""),
+    IRELAND("icons/flags/Ireland.png",""),
+    ISRAEL("icons/flags/Israel.png",""),
+    ITALY("icons/flags/Italy.png",""),
+    JAMAICA("icons/flags/Jamaica.png",""),
+    JAPAN("icons/flags/Japan.png",""),
+    JORDAN("icons/flags/Jordan.png",""),
+    KAZAKHSTAN("icons/flags/Kazakhstan.png",""),
+    KENYA("icons/flags/Kenya.png",""),
+    KIRIBATI("icons/flags/Kiribati.png",""),
+    KOREA_NORTH("icons/flags/icons/flags/Korea-North.png",""),
+    KOREA_SOUTH("icons/flags/icons/flags/Korea-South.png",""),
+    KOSOVO("icons/flags/Kosovo.png",""),
+    KUWAIT("icons/flags/Kuwait.png",""),
+    KYRGYZSTAN("icons/flags/Kyrgyzstan.png",""),
+    LAOS("icons/flags/Laos.png",""),
+    LATVIA("icons/flags/Latvia.png",""),
+    LEBANON("icons/flags/Lebanon.png",""),
+    LESOTHO("icons/flags/Lesotho.png",""),
+    LIBERIA("icons/flags/Liberia.png",""),
+    LIBYA("icons/flags/Libya.png",""),
+    LIECHTENSTEIN("icons/flags/Liechtenstein.png",""),
+    LITHUANIA("icons/flags/Lithuania.png",""),
+    LUXEMBOURG("icons/flags/Luxembourg.png",""),
+    MACEDONIA("icons/flags/Macedonia.png",""),
+    MADAGASCAR("icons/flags/Madagascar.png",""),
+    MALAWI("icons/flags/Malawi.png",""),
+    MALAYSIA("icons/flags/Malaysia.png",""),
+    MALDIVES("icons/flags/Maldives.png",""),
+    MALI("icons/flags/Mali.png",""),
+    MALTA("icons/flags/Malta.png",""),
+    MARSHALL_ISLANDS("icons/flags/Marshall-Islands.png",""),
+    MAURITANIA("icons/flags/Mauritania.png",""),
+    MAURITIUS("icons/flags/Mauritius.png",""),
+    MEXICO("icons/flags/Mexico.png",""),
+    MICRONESIA_FEDERATED("icons/flags/Micronesia-(Federated).png",""),
+    MOLDOVA("icons/flags/Moldova.png",""),
+    MONACO("icons/flags/Monaco.png",""),
+    MONGOLIA("icons/flags/Mongolia.png",""),
+    MONTENEGRO("icons/flags/Montenegro.png",""),
+    MOROCCO("icons/flags/Morocco.png",""),
+    MOZAMBIQUE("icons/flags/Mozambique.png",""),
+    MYANMAR("icons/flags/Myanmar.png",""),
+    NAMIBIA("icons/flags/Namibia.png",""),
+    NAURU("icons/flags/Nauru.png",""),
+    NEPAL("icons/flags/Nepal.png",""),
+    NETHERLANDS("icons/flags/Netherlands.png",""),
+    NEW_ZEALAND("icons/flags/New-Zealand.png",""),
+    NICARAGUA("icons/flags/Nicaragua.png",""),
+    NIGER("icons/flags/Niger.png",""),
+    NIGERIA("icons/flags/Nigeria.png",""),
+    NORWAY("icons/flags/Norway.png",""),
+    OMAN("icons/flags/Oman.png",""),
+    PAKISTAN("icons/flags/Pakistan.png",""),
+    PALAU("icons/flags/Palau.png",""),
+    PANAMA("icons/flags/Panama.png",""),
+    PAPUA_NEW_GUINEA("icons/flags/Papua-New-Guinea.png",""),
+    PARAGUAY("icons/flags/Paraguay.png",""),
+    PERU("icons/flags/Peru.png",""),
+    PHILIPPINES("icons/flags/Philippines.png",""),
+    POLAND("icons/flags/Poland.png",""),
+    PORTUGAL("icons/flags/Portugal.png",""),
+    QATAR("icons/flags/Qatar.png",""),
+    ROMANIA("icons/flags/Romania.png",""),
+    RUSSIA("icons/flags/Russia.png",""),
+    RWANDA("icons/flags/Rwanda.png",""),
+    SAINT_KITTS_AND_NEVIS("icons/flags/Saint-Kitts-and-Nevis.png",""),
+    SAINT_LUCIA("icons/flags/Saint-Lucia.png",""),
+    SAINT_VINCENT_AND_THE_GRENADINES("icons/flags/Saint-Vincent-and-the-Grenadines.png",""),
+    SAMOA("icons/flags/Samoa.png",""),
+    SAN_MARINO("icons/flags/San-Marino.png",""),
+    SAO_TOME_AND_PRINCIPE("icons/flags/Sao-Tome-and-Principe.png",""),
+    SAUDI_ARABIA("icons/flags/Saudi-Arabia.png",""),
+    SENEGAL("icons/flags/Senegal.png",""),
+    SERBIA("icons/flags/Serbia.png",""),
+    SEYCHELLES("icons/flags/Seychelles.png",""),
+    SIERRA_LEONE("icons/flags/Sierra-Leone.png",""),
+    SINGAPORE("icons/flags/Singapore.png",""),
+    SLOVAKIA("icons/flags/Slovakia.png",""),
+    SLOVENIA("icons/flags/Slovenia.png",""),
+    SOLOMON_ISLANDS("icons/flags/Solomon-Islands.png",""),
+    SOMALIA("icons/flags/Somalia.png",""),
+    SOUTH_AFRICA("icons/flags/South-Africa.png",""),
+    SOUTH_SUDAN("icons/flags/South-Sudan.png",""),
+    SPAIN("icons/flags/Spain.png",""),
+    SRI_LANKA("icons/flags/Sri-Lanka.png",""),
+    SUDAN("icons/flags/Sudan.png",""),
+    SURINAME("icons/flags/Suriname.png",""),
+    SWAZILAND("icons/flags/Swaziland.png",""),
+    SWEDEN("icons/flags/Sweden.png",""),
+    SWITZERLAND("icons/flags/Switzerland.png",""),
+    SYRIA("icons/flags/Syria.png",""),
+    TAIWAN("icons/flags/Taiwan.png",""),
+    TAJIKISTAN("icons/flags/Tajikistan.png",""),
+    TANZANIA("icons/flags/Tanzania.png",""),
+    THAILAND("icons/flags/Thailand.png",""),
+    TOGO("icons/flags/Togo.png",""),
+    TONGA("icons/flags/Tonga.png",""),
+    TRINIDAD_AND_TOBAGO("icons/flags/Trinidad-and-Tobago.png",""),
+    TUNISIA("icons/flags/Tunisia.png",""),
+    TURKEY("icons/flags/Turkey.png",""),
+    TURKMENISTAN("icons/flags/Turkmenistan.png",""),
+    TUVALU("icons/flags/Tuvalu.png",""),
+    UGANDA("icons/flags/Uganda.png",""),
+    UKRAINE("icons/flags/Ukraine.png",""),
+    UNITED_ARAB_EMIRATES("icons/flags/United-Arab-Emirates.png",""),
+    UNITED_KINGDOM("icons/flags/United-Kingdom.png",""),
+    UNITED_STATES_OF_AMERICA("icons/flags/United-States-of-America.png",""),
+    URUGUAY("icons/flags/Uruguay.png",""),
+    UZBEKISTAN("icons/flags/Uzbekistan.png",""),
+    VANUATU("icons/flags/Vanuatu.png",""),
+    VATICAN_CITY("icons/flags/Vatican-City.png",""),
+    VENEZUELA("icons/flags/Venezuela.png",""),
+    VIETNAM("icons/flags/Vietnam.png",""),
+    YEMEN("icons/flags/Yemen.png",""),
+    ZAMBIA("icons/flags/Zambia.png",""),
+    ZIMBABWE("icons/flags/Zimbabwe.png","");
+
+    private String url;
+    private String alt;
+
+    CountryFlags(String url, String alt) {
+        this.url = url;
+        this.alt = alt;
+    }
+
+    public Image create() {
+        Image image = new Image(url, alt);
+        image.getStyle().set("vertical-align", "middle");
+        return image;
+    }
+
+    /**
+     * Addes some margin to the right
+     * 
+     * @param marginRight Amount in px
+     * @return
+     */
+    public Image create(int marginRight) {
+        Image image = create();
+        image.getStyle().set("margin-right", marginRight + "px");
+        return image;
+    }
+
+    public Image create(int marginRight, int width, int height) {
+        Image image = create();
+        image.setWidth(width + "px");
+        image.setHeight(height + "px");
+        image.getStyle().set("margin-right", marginRight + "px");
+        return image;
+    }
+
+}
